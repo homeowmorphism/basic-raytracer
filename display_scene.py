7921aae58b8485fd9da9bb3755721fdb60b719b4
homeowmorphism <homeowmorphism@gmail.com>
2017-08-08 23:56:40 -0400

    refactored scene into class

--- display_scene.py
+++ display_scene.py
@@ -6,18 +6,18 @@ from src.colors import *
 from src.props.solids import Sphere 
 from src.props.lights import Sky 
 from src.props.walls import Floor 
-from src.scene import shoot_rays 
+from src.scene import Scene 
 
 sky = Sky(white,1000)
 sphere = Sphere(center=vect(-30,0,300),radius=50, albedo = 1*vect(255, 235, 49))
 sphere2 = Sphere(center=vect(80,50,200),radius=70, albedo = 1*vect(255, 61, 73))
 sphere3 = Sphere(center=vect(-30,-50,180),radius=50, albedo = 1*vect(142, 44, 232))
 floor = Floor(-500, albedo=(0.9*white))
-scene_objects = [sky, sphere, sphere2, sphere3, floor]
+scene = Scene([sky, sphere, sphere2, sphere3, floor])
 resolution = (100,100) #in pixels
 depth = 100 #in pixels
 iteration_by_pixel = 1 
 
-shoot_rays(scene_objects, resolution[0], resolution[1], depth, 100)
+scene.shoot_rays(resolution[0], resolution[1], depth, iteration_by_pixel)
 
 
--- src/scene.py
+++ src/scene.py
@@ -17,72 +17,74 @@ def random_direction(normal):
     else:
         return -1*unit_random_vector
 
-# this needs refactoring
-def ray_trace(origin, ray, scene):
+class Scene(object):
+    def __init__(self, props):
+        self.props = props
 
-    ray = ray.normalize()
+    def ray_trace(self, origin, direction):
+        direction = direction.normalize()
 
-    distances = []
-    intersections = []
+        distances = []
+        intersections = []
 
-    for prop in scene:
-        prop_intersection = prop.find_intersection(origin, ray)
-        intersections.append(prop_intersection)
+        for prop in self.props:
+            prop_intersection = prop.find_intersection(origin, direction)
+            intersections.append(prop_intersection)
 
-        if prop_intersection is None:
-            distance_from_prop = float("inf")  
-        else:
-            distance_from_prop = (prop_intersection-origin).norm()
+            if prop_intersection is None:
+                distance_from_prop = float("inf")  
+            else:
+                distance_from_prop = (prop_intersection-origin).norm()
 
-        distances.append(distance_from_prop)
+            distances.append(distance_from_prop)
 
-    if all(intersection is None for intersection in intersections):
-        return black
+        if all(intersection is None for intersection in intersections):
+            return black
 
-    else: 
-        index_closest_prop = np.argmin(distances)
-        closest_prop = scene[index_closest_prop]
+        else: 
+            index_closest_prop = np.argmin(distances)
+            closest_prop = self.props[index_closest_prop]
 
-        if isinstance(closest_prop, Light):
-            normal = closest_prop.find_normal(prop_intersection)
-            return closest_prop.color
+            if isinstance(closest_prop, Light):
+                normal = closest_prop.find_normal(prop_intersection)
+                return closest_prop.color
 
-        else:
-            closest_intersection = intersections[index_closest_prop]
+            else:
+                closest_intersection = intersections[index_closest_prop]
 
-            albedo = closest_prop.albedo
-            normal = closest_prop.find_normal(closest_intersection)
+                albedo = closest_prop.albedo
+                normal = closest_prop.find_normal(closest_intersection)
 
-            new_origin = closest_intersection
-            reflected_ray = random_direction(normal)
+                new_origin = closest_intersection
+                reflected_ray = random_direction(normal)
 
-            lambertian = reflected_ray.inner(normal)
+                lambertian = reflected_ray.inner(normal)
 
-            epsilon = 1
+                epsilon = 1
 
-            return lambertian*albedo.compwise_mult(ray_trace(new_origin + (epsilon*reflected_ray), reflected_ray, scene))
+                return lambertian*albedo.compwise_mult(self.ray_trace(new_origin + (epsilon*reflected_ray), reflected_ray))
 
-def shoot_rays(scene, screen_width, screen_height, screen_depth, iteration_by_pixel):
-    screen = initialize_screen(screen_width, screen_height)
-    screen_center = find_screen_center(screen_width, screen_height, screen_depth)
+    def shoot_rays(self, screen_width, screen_height, screen_depth, iteration_by_pixel):
+        screen = initialize_screen(screen_width, screen_height)
+        screen_center = find_screen_center(screen_width, screen_height, screen_depth)
 
-    for i in range(screen_width):
-        for j in range(screen_height):
-            relative_position = vect(i,-j,0)
-            absolute_position = find_absolute_screen_position(relative_position, screen_center)
-            ray = absolute_position
-            sum_of_colors = vect(0,0,0)
-            total_intensity = 0
+        for i in range(screen_width):
+            for j in range(screen_height):
+                relative_position = vect(i,-j,0)
+                absolute_position = find_absolute_screen_position(relative_position, screen_center)
+                direction = absolute_position
+                sum_of_colors = vect(0,0,0)
+                total_intensity = 0
 
-            for k in range(iteration_by_pixel):
-                color = ray_trace(absolute_position, ray, scene)
-                sum_of_colors += color
+                for k in range(iteration_by_pixel):
+                    color = self.ray_trace(absolute_position, direction)
+                    sum_of_colors += color
 
-            average_color = sum_of_colors/iteration_by_pixel
-            
-            write_screen_pixel(screen, relative_position, average_color) 
-    
-    display_image(screen)
+                average_color = sum_of_colors/iteration_by_pixel
+                
+                write_screen_pixel(screen, relative_position, average_color) 
+        
+        display_image(screen)
 
 
 
