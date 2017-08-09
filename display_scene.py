21e631bca1f67f376ca3fa7a9c2a0777c945c22e
homeowmorphism <homeowmorphism@gmail.com>
2017-08-08 22:36:42 -0400

    folded the code into directories! works

--- display_scene.py
+++ display_scene.py
@@ -1,14 +1,12 @@
-from vector import Vector as vect
 import numpy as np
 import scipy.misc as smp
 
-from sphere import *
-from color import *
-from screen import *
-from raytrace import *
-from colorlib import *
-from light import *
-from floor import *
+from src.vector import Vector as vect
+from src.colors import *
+from src.props.solids import Sphere 
+from src.props.lights import Sky 
+from src.props.walls import Floor 
+from src.scene import shoot_rays 
 
 sky = Sky(white,1000)
 sphere = Sphere(center=vect(-30,0,300),radius=50, albedo = 1*vect(255, 235, 49))
@@ -18,7 +16,7 @@ floor = Floor(-500, albedo=(0.9*white))
 scene_objects = [sky, sphere, sphere2, sphere3, floor]
 resolution = (100,100) #in pixels
 depth = 100 #in pixels
-iteration_by_pixel = 5
+iteration_by_pixel = 1 
 
 shoot_rays(scene_objects, resolution[0], resolution[1], depth, 100)
 
